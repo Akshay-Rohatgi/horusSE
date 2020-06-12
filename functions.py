185c3b93ac5afafefe303727fa3ed77686aef19e
Akshay-Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-06-12 12:02:17 -0700

    added more checks

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import os.path
+from os import path
 
 def String_In_File(path, string):
     with open(path, 'r') as handle:
@@ -26,3 +28,25 @@ def Package_Not_Installed(package):
     if package in handle:
         return False
     return True
+
+def Firewall_Up():
+    handle = subprocess.getoutput("sudo ufw status | grep 'Status: active'")
+    if handle == 'Status: active':
+        return True
+    else:
+        return False
+
+def File_Exists(path):
+    if os.path.exists(path):
+        return True
+    return False
+
+def File_Not_Exists(path):
+    if os.path.exists(path):
+        return False
+    return True
+
+def User_Exists(username)
+    handle = subprocess.getoutput("")
+
+print(File_Not_Exists('/etc/login.defs'))
